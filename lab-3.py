4b570d18a0b3adba8da52195ae68bed427674417
alex-ubbcluj <alexandru.gruia1@ubbcluj.ro>
2021-10-22 02:01:33 +0300

    second commit

--- lab-3.py
+++ lab-3.py
@@ -22,13 +22,24 @@ def is_palindrome(n: int) -> bool:
     return False
 
 
+def only_palindromes(lst: list) -> bool:
+    #verifica daca toate numerele din lst sunt palindroame
+    for i in lst:
+        if is_palindrome(i) == False: return False
+    return True
+
+
 def get_longest_all_palindromes(lst: list) -> list:
     """
     determina cea mai lunga secventa dintr-o lista in care toate numerele sunt palindroame
     param. lst - lista
     return - o lista ce contine cea mai lunga secventa in care toate numerele sunt palindroame
     """
-    pass
+    max_seq = []
+    for i in range(len(lst)):
+        for j in range(i, len(lst)):
+            if len(lst[i: j+1]) > len(max_seq) and only_palindromes(lst[i: j+1]) == True: max_seq = lst[i: j+1]
+    return max_seq
 
 
 def test_get_longest_all_palindromes():
@@ -54,20 +65,45 @@ def test_get_longest_powers_of_k():
 test_get_longest_powers_of_k()
 
 
+def isprime(x: int) -> bool:
+    """
+    determina daca un nr intreg e prim
+    param. x - un numar intreg
+    return - True daca x e prim, False in caz contrar
+    """
+    if x < 2: return False
+    for i in range(2, x//2+1):
+        if x % i == 0: return False
+    return True
+
+
+def get_longest_all_primes(lst: list) -> list:
+    pass
+
+
+def test_get_longest_all_primes():
+    pass
+
+
+test_get_longest_all_primes()
+
+
 def main():
     l = []
     while True:
         print("1. Citire date.")
         print("2. Afisare cea mai lungă subsecvență cu proprietatea de la ex. 5")
         print("3. Afisare cea mai lungă subsecvență cu proprietatea de la ex. 15")
-        print("4. Ieșire.")
+        print("4. Afisare cea mai lungă subsecvență cu proprietatea de la ex. 2")
+        print("5. Ieșire.")
         option = int(input("Selectati optiunea: "))
         if option == 1: l = citire_lista()
         elif option == 2: print(get_longest_all_palindromes(l))
         elif option == 3:
             k = input("Dati valoarea k: ")
             print(get_longest_powers_of_k(l, k))
-        elif option == 4: break
+        elif option == 4: print(get_longest_all_primes(l))
+        elif option == 5: break
         else: print("Optiune invalida")
 
 
